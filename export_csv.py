4fdaba4434cae7e6fdc407b8604e4fe58994f548
njupalli <nehajupalli06@gmail.com>
2025-04-28 13:05:37 -0400

    added comments

--- export_csv.py
+++ export_csv.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 def export_risk_path_to_csv(sites, start, stop, output_file):
+    #Stacks parameters vertically in vstack
     all_data = np.vstack([sites, start, stop])
+    #converts to a pandas DataFrame from numpy
     df = pd.DataFrame(all_data)
+    #writes DataFrame as a CSV file
     df.to_csv(output_file, header=False, index=False)
--- load_risk_path_from_csv.py
+++ load_risk_path_from_csv.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
 def load_risk_path_from_csv(csv_file):
+    #converts to pandas DataFrame
     df = pd.read_csv(csv_file, header=None)
+    #takes in all rows except last two
     sites = df.iloc[:-2].values.astype(float)
+    #represents starting points and gets the second to last row
     start = np.array(df.iloc[-2].values, dtype=float)
+    #gets the last row representing the endpoint
     stop = np.array(df.iloc[-1].values, dtype=float)
 
     return sites, start, stop
